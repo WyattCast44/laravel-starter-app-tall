962e2e74dc030d70694d0b638717701b11b89306
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2021-01-06 18:18:21 -0800

    wip

--- routes/auth.php
+++ routes/auth.php
@@ -3,10 +3,12 @@
 use App\Http\Livewire\Auth\Login;
 use App\Http\Livewire\Auth\Register;
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\Auth\VerifyEmail;
 use App\Http\Livewire\Auth\ResetPassword;
 use App\Http\Livewire\Auth\ForgotPassword;
 use App\Http\Livewire\Auth\ConfirmPassword;
 use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\EmailVerificationController;
 
 Route::group(['middleware' => ['guest']], function () {
     Route::get('/login', Login::class)->name('login');
@@ -17,5 +19,7 @@ Route::group(['middleware' => ['guest']], function () {
 
 Route::group(['middleware' => ['auth']], function () {
     Route::post('/logout', LogoutController::class)->name('logout');
+    Route::get('/email/verify', VerifyEmail::class)->name('verification.notice');
     Route::get('/confirm-password', ConfirmPassword::class)->name('password.confirm');
+    Route::get('/email/verify/{id}/{hash}', EmailVerificationController::class)->name('verification.verify');
 });
