da01b159b47b9a7058be0c3bcc0c171fcd99c712
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2021-01-06 18:25:08 -0800

    wip

--- resources/views/layouts/marketing.blade.php
+++ resources/views/layouts/marketing.blade.php
@@ -5,8 +5,14 @@
     <div class="text-center py-2 mb-2 border-b">
         <h1>Marketing.blade.php</h1>
         <div class="flex items-center space-x-2 justify-center">
-            <a href="{{ route('login') }}">Login</a>
-            <a href="{{ route('register') }}">Register</a>
+
+            @if (auth()->check())
+                <a href="{{ route('dashboard') }}">Dashboard</a>
+            @else
+                <a href="{{ route('login') }}">Login</a>
+                <a href="{{ route('register') }}">Register</a>
+            @endif
+            
         </div>
     </div>
         
